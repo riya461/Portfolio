5cc11deea9795a362a83bdf4b726e500d1ede5dd
Riya Sabu <riyasabures@gmail.com>
2022-10-25 04:24:52 +0530

    first draft

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+navigation: {
+    nextE1: '.swiper-button-next',
+    prevE1: '.swiper-button-prev',
+},
+pagination: {
+    e1: '.swiper-pagination'
+},
+mousewheel: true,
+keyboard: true,
+});
